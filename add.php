c17b0d83c519d961fd74b7c145c68f79b893995a
Farsang Benedek <carnevalben@gmail.com>
2025-02-04 07:51:23 +0100

    Update add.php

--- add.php
+++ add.php
@@ -9,7 +9,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["cim"])) {
 
     if (!empty($cim)) {
         try {
-            $stmt = $conn->prepare("INSERT INTO Ugyek (cim) VALUES (:cim)");
+            $stmt = $conn->prepare("INSERT INTO ugyek (cim) VALUES (:cim)");
             $stmt->bindParam(':cim', $cim, PDO::PARAM_STR);
             $stmt->execute();
             echo "Sikeres mentés!";
